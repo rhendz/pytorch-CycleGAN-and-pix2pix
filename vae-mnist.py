8ff869d0c4ab6fbfd0b3412dcb7b95159088e6d9
rhendz <patelankush.2012@gmail.com>
2022-05-15 18:56:11 -0400

    update vae-mnist

--- vae-mnist.py
+++ vae-mnist.py
@@ -21,8 +21,9 @@ from data import create_dataset
 from models import create_model
 from util.visualizer import Visualizer
 from argparse import Namespace
+from models.networks import *
 
-# Set options for VAE
+# Set options for CycleGAN network
 opt = Namespace(batch_size=1, beta1=0.5, checkpoints_dir='./checkpoints', continue_train=False, crop_size=28, 
                dataroot='./datasets/apple2orange_resized_64', dataset_mode='unaligned', direction='AtoB', 
                display_env='main', display_freq=400, display_id=1, display_ncols=4, display_port=8097, 
@@ -36,15 +37,205 @@ opt = Namespace(batch_size=1, beta1=0.5, checkpoints_dir='./checkpoints', contin
                save_epoch_freq=5, save_latest_freq=5000, serial_batches=False, suffix='', 
                update_html_freq=1000, use_wandb=False, verbose=False)
 
-# train_loader = create_dataset(opt)
-# # print(len(train))
-# opt.phase='test'
-# test_loader = create_dataset(opt)
-# # print(len(test))
-# # m = len(dataset)
-
-# # for i_batch, sample_batched in enumerate(dataset):
-# #   print(sample_batched)
-
+# Setup torch training device
 device = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
 print(f'Selected device: {device}')
+
+# Setup MNIST dataset
+def get_mnist_dataset():
+    data_dir = 'dataset'
+
+    train_dataset = torchvision.datasets.MNIST(data_dir, train=True, download=True)
+    test_dataset  = torchvision.datasets.MNIST(data_dir, train=False, download=True)
+
+    return train_dataset, test_dataset
+
+def create_mnist_dataset(train_dataset, test_dataset):
+    train_transform = transforms.Compose([
+    transforms.ToTensor(),
+    ])
+
+    test_transform = transforms.Compose([
+    transforms.ToTensor(),
+    ])
+
+    train_dataset.transform = train_transform
+    test_dataset.transform = test_transform
+
+    m=len(train_dataset)
+
+    train_data, val_data = random_split(train_dataset, [int(m-m*0.2), int(m*0.2)])
+    batch_size=32
+
+    train_loader = torch.utils.data.DataLoader(train_data, batch_size=batch_size)
+    valid_loader = torch.utils.data.DataLoader(val_data, batch_size=batch_size)
+    test_loader = torch.utils.data.DataLoader(test_dataset, batch_size=batch_size,shuffle=True)
+    return train_loader, valid_loader, test_loader
+
+class VariationalEncoder(nn.Module):
+    def __init__(self, latent_dims):  
+        super(VariationalEncoder, self).__init__()
+        nc = 1 # num channels
+        self.conv1 = nn.Conv2d(nc, 8, 3, stride=2, padding=1)
+        self.conv2 = nn.Conv2d(8, 16, 3, stride=2, padding=1)
+        self.batch2 = nn.BatchNorm2d(16)
+        self.conv3 = nn.Conv2d(16, 32, 3, stride=2, padding=0)  
+        self.linear1 = nn.Linear(32*3*3, 128)
+        self.linear2 = nn.Linear(128, latent_dims)
+        self.linear3 = nn.Linear(128, latent_dims)
+
+        self.N = torch.distributions.Normal(0, 1)
+        if torch.cuda.is_available():
+            self.N.loc = self.N.loc.cuda() # hack to get sampling on the GPU
+            self.N.scale = self.N.scale.cuda()
+        self.kl = 0
+
+    def forward(self, x):
+        x = x.to(device)
+        x = F.relu(self.conv1(x))
+        x = F.relu(self.batch2(self.conv2(x)))
+        x = F.relu(self.conv3(x))
+        # print(x.shape)
+        x = torch.flatten(x, start_dim=1)
+        x = F.relu(self.linear1(x))
+        mu =  self.linear2(x)
+        sigma = torch.exp(self.linear3(x))
+        z = mu + sigma*self.N.sample(mu.shape)
+        self.kl = (sigma**2 + mu**2 - torch.log(sigma) - 1/2).sum()
+        return z
+
+# Decoder model replicated after generator in CycleGAN
+class Decoder(nn.Module):
+  def __init__(self, latent_dims):
+        super().__init__()
+
+        self.decoder_lin = nn.Sequential(
+            nn.Linear(latent_dims, 128),
+            nn.ReLU(True),
+            nn.Linear(128, 3 * 3 * 32),
+            nn.ReLU(True)
+        )
+
+        self.unflatten = nn.Unflatten(dim=1, unflattened_size=(32, 3, 3))
+
+        self.decoder_conv = nn.Sequential(
+            nn.ConvTranspose2d(32, 16, 3, stride=2, output_padding=0),
+            nn.BatchNorm2d(16),
+            nn.ReLU(True),
+            nn.ConvTranspose2d(16, 8, 3, stride=2, padding=1, output_padding=1),
+            nn.BatchNorm2d(8),
+            nn.ReLU(True),
+            nn.ConvTranspose2d(8, 1, 3, stride=2, padding=1, output_padding=1)
+        )
+
+        self.generator = define_G(opt.input_nc, opt.output_nc, opt.ngf, opt.netG, opt.norm, not opt.no_dropout, opt.init_type, opt.init_gain)
+
+  def forward(self, x):
+        x = self.decoder_lin(x)
+        x = self.unflatten(x)
+        x = self.decoder_conv(x)
+        x = self.generator(x)
+        x = torch.sigmoid(x)
+        return x
+
+class VariationalAutoencoder(nn.Module):
+    def __init__(self, latent_dims):
+        super(VariationalAutoencoder, self).__init__()
+        self.encoder = VariationalEncoder(latent_dims)
+        self.decoder = Decoder(latent_dims)
+
+    def forward(self, x):
+        x = x.to(device)
+        z = self.encoder(x)
+        return self.decoder(z)
+
+# Training function
+def train_epoch(vae, device, dataloader, optimizer):
+    # Set train mode for both the encoder and the decoder
+    vae.train()
+    train_loss = 0.0
+    # Iterate the dataloader (we do not need the label values, this is unsupervised learning)
+    for x, _ in dataloader: 
+        # print(x)
+        # Move tensor to the proper device
+        x = x.to(device)
+        x_hat = vae(x)
+        # Evaluate loss
+        loss = ((x - x_hat)**2).sum() + vae.encoder.kl
+
+        # Backward pass
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+        # Print batch loss
+        # print('\t partial train loss (single batch): %f' % (loss.item()))
+        train_loss+=loss.item()
+
+    return train_loss / len(dataloader.dataset)
+
+# Testing function
+def test_epoch(vae, device, dataloader):
+    # Set evaluation mode for encoder and decoder
+    vae.eval()
+    val_loss = 0.0
+    with torch.no_grad(): # No need to track the gradients
+        for x, _ in dataloader:
+            # Move tensor to the proper device
+            x = x.to(device)
+            # Encode data
+            encoded_data = vae.encoder(x)
+            # Decode data
+            x_hat = vae(x)
+            loss = ((x - x_hat)**2).sum() + vae.encoder.kl
+            val_loss += loss.item()
+
+    return val_loss / len(dataloader.dataset)
+
+# Plot reconstructed examples during training
+def plot_ae_outputs(encoder,decoder,test_dataset,n=10):
+    plt.figure(figsize=(16,4.5))
+    targets = test_dataset.targets.numpy()
+    t_idx = {i:np.where(targets==i)[0][0] for i in range(n)}
+    for i in range(n):
+      ax = plt.subplot(2,n,i+1)
+      img = test_dataset[t_idx[i]][0].unsqueeze(0).to(device)
+      encoder.eval()
+      decoder.eval()
+      with torch.no_grad():
+         rec_img  = decoder(encoder(img))
+      plt.imshow(img.cpu().squeeze().numpy(), cmap='gist_gray')
+      ax.get_xaxis().set_visible(False)
+      ax.get_yaxis().set_visible(False)  
+      if i == n//2:
+        ax.set_title('Original images')
+      ax = plt.subplot(2, n, i + 1 + n)
+      plt.imshow(rec_img.cpu().squeeze().numpy(), cmap='gist_gray')  
+      ax.get_xaxis().set_visible(False)
+      ax.get_yaxis().set_visible(False)  
+      if i == n//2:
+         ax.set_title('Reconstructed images')
+    plt.show()  
+
+# Test construction of VAE model
+# vae = VariationalAutoencoder(16)
+# summary(vae, input_size=(8,1,28,28))
+
+# Set the random seed for reproducible results
+torch.manual_seed(0)
+
+# Construct VAE and optimizer and load onto torch device
+vae = VariationalAutoencoder(latent_dims=4)
+optim = torch.optim.Adam(vae.parameters(), lr=1e-3, weight_decay=1e-5)
+vae.to(device)
+
+# Train VAE
+train_dataset, test_dataset = get_mnist_dataset()
+train_loader, test_loader, valid_loader = create_mnist_dataset(train_dataset, test_dataset)
+num_epochs = 100
+vae_encoder = 0
+vae_decoder = 0
+for epoch in range(num_epochs):
+    train_loss = train_epoch(vae,device,train_loader,optim)
+    val_loss = test_epoch(vae,device,valid_loader)
+    print('\n EPOCH {}/{} \t train loss {:.3f} \t val loss {:.3f}'.format(epoch + 1, num_epochs,train_loss,val_loss))
+    plot_ae_outputs(vae.encoder,vae.decoder,test_dataset,n=10)
